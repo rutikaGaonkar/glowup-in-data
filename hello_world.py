cb67f128ce5fdeed9c0dadbd6446c88c46f1b063
Rutika Gaonkar <rutika.gaonkar1426@gmail.com>
2025-06-13 13:19:49 +0100

    hello_world.py
    the very first code of every coder

---
+++ hello_world.py
@@ -0,0 +1,4 @@
+Python 3.13.3 (v3.13.3:6280bb54784, Apr  8 2025, 10:47:54) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> print("Hello, World!")
+Hello, World!
